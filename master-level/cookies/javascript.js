3aaaf8522c43d89e4fc4dd994a3ed3946d75a22c
William Andres Ramirez Jimenez <frivandres2011@hotmail.com>
2025-03-18 22:48:54 -0500

    WR: Update.

---
+++ master-level/cookies/javascript.js
@@ -0,0 +1,9 @@
+// ______________________Cookies__________________________
+// - Definición y usos comunes (diferenciar users, UX, Ads, etc...) -> .
+// - Formas de clasificar cookies -> .
+// - Crear una cookie -> .
+// - Method encodeURIComponent() y decodeURIComponent() -> .
+// - Leer una cookie -> .
+// - Modificar una cookie -> .
+// - Borrar una cookie -> .
+// - Ejemplo de uso (Aviso de uso de cookies, cumplimiento con el RGPD y la ePrivacy) -> .
